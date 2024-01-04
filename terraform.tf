97ad30c5c0e0147d64feabd61e266b1a4fa82f9e
Joan Mendez <jmendez@freedomh.com>
2024-01-04 16:07:43 -0500

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "learnathome"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/git
   required_providers {
     aws = {
       source  = "hashicorp/aws"
